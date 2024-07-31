189bc4bc067342ed009e5380980f4e25647b858b
bobby.li <bobby.lhan@gmail.com>
2024-07-31 19:53:49 +0800

    feat: support vue3 sfc build

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,25 @@
 import path from 'path';
+import * as crypto from 'crypto';
 import through2 from 'through2';
 import PluginError from 'plugin-error';
-import { createDefaultCompiler, assemble } from '@vue/component-compiler';
+import { parse, compileScript, compileTemplate, compileStyle } from '@vue/compiler-sfc';
 import replaceExt from '@utils/replaceExt';
 import normalize from '@utils/normalize';
 
-import type { ScriptOptions, StyleOptions, TemplateOptions } from '@vue/component-compiler';
+import type { SFCBlock, SFCScriptCompileOptions, SFCStyleCompileOptions, SFCTemplateCompileOptions } from '@vue/compiler-sfc';
 
 const applySourceMap = require('vinyl-sourcemaps-apply');
 const PLUGIN_NAME = 'vue-sfc';
 
+type SFCMap = Exclude<SFCBlock['map'], undefined>;
+
 export default function plugin(opts?: {
   ext?: string;
-  script?: ScriptOptions;
-  style?: StyleOptions;
-  template?: TemplateOptions;
-  normalizer?: string;
-  styleInjector?: string;
-  styleInjectorSSR?: string;
+  script?: SFCScriptCompileOptions;
+  style?: SFCStyleCompileOptions;
+  template?: SFCTemplateCompileOptions;
 }) {
-  const { ext, script, style, template, normalizer, styleInjector, styleInjectorSSR } = opts || {};
-  const vueCompiler = createDefaultCompiler({ script, style, template });
+  const { ext = '.js', script, style, template } = opts || {};
 
   return through2.obj(function (file, enc, next) {
     if (file.extname !== '.vue') {
@@ -33,29 +32,80 @@ export default function plugin(opts?: {
       if (file.isBuffer()) {
         try {
           const content = file.contents.toString(enc);
-          const localPath = path.relative(file.cwd, file.path);
-          const result = assemble(
-            vueCompiler,
-            localPath,
-            vueCompiler.compileToDescriptor(localPath, content),
-            { normalizer, styleInjector, styleInjectorSSR }
+          const { descriptor, errors } = parse(content, { filename: file.path });
+          const id = crypto.createHash('md5').update(`${descriptor.filename}-${new Date().getTime()}-${Math.random()}`).digest('hex');
+          if (errors.length) {
+            errors.forEach(err => this.emit('error', new PluginError(PLUGIN_NAME, err)));
+            return next();
+          }
+
+          let scriptResult = '', scriptMap: SFCMap | undefined;
+          if (descriptor.script) {
+            const { content, map } = compileScript(descriptor, { id, ...script });
+            scriptResult = content;
+            scriptMap = map;
+          }
+
+          let templateResult = '', templateMap: SFCMap | undefined;
+          if (descriptor.template) {
+            const { code, map } = compileTemplate({
+              ...template!,
+              id,
+              source: descriptor.template.content,
+              filename: file.path,
+            });
+            templateResult = code;
+            templateMap = map;
+          }
+
+          let styleResult = '';
+          const styleMap: SFCMap[] = [];
+          if (descriptor.styles.length) {
+            descriptor.styles.forEach(stylePart => {
+              if (stylePart.content) {
+                const { code, map } = compileStyle({
+                  ...style!,
+                  id,
+                  source: stylePart.content,
+                  filename: file.path,
+                });
+                styleResult += code;
+                map && styleMap.push(map);
+              }
+            });
+          }
+
+          file.contents = Buffer.from(
+            scriptResult + templateResult + styleResult
           );
-
-
-          file.contents = Buffer.from(result.code);
-          file.path = replaceExt(file.path, ext || '.js');
+          file.path = replaceExt(file.path, ext);
 
           if (file.sourceMap) {
-            const { map } = result;
-            if (!map.sourcesContent) map.sourcesContent = [content];
-            map.sources = map.sources.map((f: string) =>
-              normalize(path.relative(file.base, path.resolve(file.cwd, f)))
-            );
-            map.file = normalize(file.relative);
+            const map = {} as SFCMap;
+            const mapArr = [] as SFCMap[];
+            if (scriptMap) mapArr.push(scriptMap);
+
+            if (templateMap) mapArr.push(templateMap);
+
+            if (styleMap.length) mapArr.push(...styleMap);
+            mapArr.map(m => {
+              const { names, sources, sourcesContent, mappings, version } = m;
+              map.sources = sources.map((f: string) =>
+                normalize(path.relative(file.base, path.resolve(file.cwd, f)))
+              );
+              if (!map.sourcesContent) map.sourcesContent = [];
+              sourcesContent && map.sourcesContent.push(...sourcesContent);
+              map.file = normalize(file.relative);
+              map.mappings += mappings;
+              if (!map.version) map.version = version;
+              if (!map.names) map.names = [];
+              map.names.push(...names);
+            });
             applySourceMap(file, map);
           }
+
           this.push(file);
-        } catch (err) {
+        } catch (err: any) {
           this.emit('error', new PluginError(PLUGIN_NAME, err));
         }
       }
